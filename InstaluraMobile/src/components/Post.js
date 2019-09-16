faabecfa9e4cd3c41659538ad3815f76d7e9b904
Junior <junior.jaava@gmail.com>
2019-09-16 19:40:26 +0200

    constructor of Post. Start to use TouchableOpacity. manipulate the state in render() function. This.state going to be foto

--- InstaluraMobile/src/components/Post.js
+++ InstaluraMobile/src/components/Post.js
@@ -1,41 +1,74 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  Dimensions
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Dimensions,
+    TouchableOpacity
 } from 'react-native';
 
 const width = Dimensions.get('screen').width;
 
 export default class Post extends Component {
-  render() {
-   return (
-      <View>
-        <View style={styles.cabecalho}>
-          <Image source={{uri: this.props.foto.urlPerfil}} style={styles.fotoDePerfil} />
-          <Text>{this.props.foto.loginUsuario}</Text>
-        </View>
-        <Image source={{uri: this.props.foto.urlFoto}} style={styles.foto} />
-      </View>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = { foto: this.props.foto }
+    }
+
+    carregaIcone(likeada) {
+        return likeada ? require('../../resources/img/s2-checked.png') : require('../../resources/img/s2.png')
+    }
+
+    like() {
+        const fotoAtualizada = {
+            ...this.state.foto,
+            likeada: !this.state.foto.likeada
+        }
+        this.setState({ foto: fotoAtualizada });
+    }
+
+    render() {
+        const { foto } = this.state;
+        return (
+            <View>
+                <View style={styles.cabecalho}>
+                    <Image source={{ uri: foto.urlPerfil }} style={styles.fotoDePerfil} />
+                    <Text>{foto.loginUsuario}</Text>
+                </View>
+                <Image source={{ uri: foto.urlFoto }} style={styles.foto} />
+
+                <View style={styles.rodape}>
+                    <TouchableOpacity onPress={this.like.bind(this)}>
+                        <Image style={styles.botaoDeLike} source={this.carregaIcone(foto.likeada)} />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  cabecalho: {
-    margin:10, 
-    flexDirection: 'row', 
-    alignItems: 'center'
-  },
-  fotoDePerfil: {
-    margin: 10, 
-    borderRadius: 20, 
-    width:40, height:40
-  },
-  foto: {
-    width:width, 
-    height:width
-  }
+    cabecalho: {
+        margin: 10,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    fotoDePerfil: {
+        margin: 10,
+        borderRadius: 20,
+        width: 40, height: 40
+    },
+    foto: {
+        width: width,
+        height: width
+    },
+    botaoDeLike: {
+        width: 40,
+        height: 40
+    },
+    rodape: {
+        margin: 10
+    }
 })
